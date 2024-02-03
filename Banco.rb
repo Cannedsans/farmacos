1efaac086101b2ae25931301c8e2554e92bc6fb0
João Felipe <joaofilipera13@gmail.com>
2024-02-03 11:39:15 -0300

    Insert_Read

--- Banco.rb
+++ Banco.rb
@@ -4,37 +4,63 @@ class Banco
   DATABASE_FILE = 'data/farmacia.db'
 
   def self.criar_banco
-    SQLite3::Database.new(DATABASE_FILE) do |db|
-      @db.execute <<-SQL 
-      CREATE TABLE IF NOT EXISTS produtos(
-        id integer PRIMARY KEY AUTOINCREMENT,
-        nome text not null,
-        preco float not null,
-        descricao text
-      );
-        CREATE TABLE IF NOT EXISTS cliente(
-            cpf VARCHAR(11) PRIMARY KEY,
-            nome TEXT NOT NULL,
-            endereco TEXT
-        );
-        CREATE TABLE IF NOT EXISTS pedido(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nomecliente TEXT NOT NULL, 
-            produto INTEGER NOT NULL,
-            criacao DATETIME DEFAULT CURRENT_TIMESTAMP
-        );
-      SQL
-    end
+    db = SQLite3::Database.new(DATABASE_FILE)
+    criar_tabelas(db)
+    db.close
   end
 
-  def inserir()
-    @db.execute()
+  def self.criar_tabelas(db)
+    db.execute <<-SQL 
+      CREATE TABLE IF NOT EXISTS produtos (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nome TEXT NOT NULL,
+          preco FLOAT NOT NULL,
+          descricao TEXT
+      );
+
+      CREATE TABLE IF NOT EXISTS cliente (
+          cpf VARCHAR(11) PRIMARY KEY,
+          nome TEXT NOT NULL,
+          endereco TEXT
+      );
+
+      CREATE TABLE IF NOT EXISTS pedido (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nomecliente TEXT NOT NULL, 
+          produto INTEGER NOT NULL,
+          criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (nomecliente) REFERENCES cliente(cpf),
+          FOREIGN KEY (produto) REFERENCES produtos(id)
+      );
+    SQL
+  end
+
+  def inserir(nome, preco, descricao)
+    begin
+      db = SQLite3::Database.new(DATABASE_FILE)
+      db.execute('INSERT INTO produtos (nome, preco, descricao) VALUES (?, ?, ?);', [nome, preco, descricao])
+      return true 
+    rescue SQLite3::Exception => e
+      puts "Erro ao inserir dados no banco de dados: #{e}"
+      return false 
+    ensure
+      db.close if db
+    end
   end 
 
-  def ler()
-    @db.execute()
-  end
-
+  def ler(tabela)
+     begin
+            db = SQLite3::Database.new(DATABASE_FILE)
+            consulta = "SELECT * FROM #{tabela};"
+            valor = db.execute(consulta)
+            return valor
+        rescue SQLite3::Exception => e
+            puts "Erro ao ler dados do banco de dados: #{e}"
+            return nil
+        ensure
+            db.close if db
+        end
+    end
 end
 
 Banco.criar_banco
