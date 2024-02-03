e60aaea4debda0926a14a4c6e3c89ce506221c89
João Felipe <joaofilipera13@gmail.com>
2024-02-03 11:05:09 -0300

    banco

---
+++ Banco.rb
@@ -0,0 +1,32 @@
+require 'sqlite3'
+
+class Banco
+  DATABASE_FILE = 'data/farmacia.db'
+
+  def self.criar_banco
+    SQLite3::Database.new(DATABASE_FILE) do |db|
+      db.execute <<-SQL 
+      CREATE TABLE IF NOT EXISTS produtos(
+        id integer PRIMARY KEY AUTOINCREMENT,
+        nome text not null,
+        preco float not null,
+        descricao text
+      );
+        CREATE TABLE IF NOT EXISTS cliente(
+            cpf VARCHAR(11) PRIMARY KEY,
+            nome TEXT NOT NULL,
+            endereco TEXT
+        );
+        CREATE TABLE IF NOT EXISTS pedido(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nomecliente TEXT NOT NULL, 
+            produto INTEGER NOT NULL,
+            criacao DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+      SQL
+    end
+  end
+
+end
+
+Banco.criar_banco
